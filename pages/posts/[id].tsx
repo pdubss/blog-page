a3193a827c57f6d93e3853a33c4dee57507ec06a
Perry <perryy41996@yahoo.com>
2022-07-18 01:45:33 -0400

    added getStaticProps and getStaticPaths to dynamic id page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -6,8 +6,60 @@
 import Layout from "../../components/Layout";
 import styles from "../../styles/Home.module.css";
 
-const postPage: React.FC = () => {
-  return <main className={styles.main}>This is the detailed post page</main>;
+type propsType = {
+  category: string;
+  id: number;
+  image: string;
+  title: string;
 };
+
+const postPage: React.FC<propsType> = (props) => {
+  console.log(props);
+  return (
+    <main className={styles.main}>
+      <h1>{props.title}</h1>
+    </main>
+  );
+};
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          id: "1",
+        },
+      },
+      {
+        params: {
+          id: "2",
+        },
+      },
+      {
+        params: {
+          id: "3",
+        },
+      },
+      {
+        params: {
+          id: "4",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const pageID = context.params.id;
+  const response = await fetch(
+    `https://blogpage-posts-default-rtdb.firebaseio.com/posts/p${pageID}.json`
+  );
+  const data = await response.json();
+
+  return {
+    props: data,
+  };
+}
 
 export default postPage;
